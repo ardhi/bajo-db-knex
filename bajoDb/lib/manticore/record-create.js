4d6bf5346e04292492f4196ea477c6ce20fc81dc
Ardhi Lukianto <ardhi@lukianto.com>
2024-04-20 15:22:06 +0700

    Updates

--- bajoDb/lib/manticore/record-create.js
+++ bajoDb/lib/manticore/record-create.js
@@ -1,9 +1,11 @@
 import sanitizeInput from './_sanitize-input.js'
 
 async function create ({ schema, body, options } = {}) {
+  const { generateId, isSet } = this.bajo.helper
   const { getInfo } = this.bajoDb.helper
   const { instance, returning } = getInfo(schema)
   const nbody = sanitizeInput.call(this, body, schema)
+  if (!isSet(nbody.id)) nbody.id = generateId('int')
   return await instance.client(schema.collName).insert(nbody, ...returning)
 }
 
